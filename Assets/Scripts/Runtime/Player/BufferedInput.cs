4cf6560698b13ceb4b0d20ac3ba77900ddea2d21
Toreole <toreole.official@gmail.com>
2022-07-20 17:51:10 +0200

    Basic Scene Setup

---
+++ Assets/Scripts/Runtime/Player/BufferedInput.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace MovementGame.Input
+{
+    /// <summary>
+    /// A simple class to buffer input. Can be implicitly cast to bool, or explicitly used via TryConsume() (the same thing but less syntax sugar)
+    /// </summary>
+    [Serializable] //needs to be serializable for Unity.
+    public class BufferedInput
+    {
+        [SerializeField, Range(0.0f, 0.4f)]
+        private float bufferDuration;
+
+        private bool active = false;
+        private float activeTime = 0.0f;
+
+        /// <summary>
+        /// Activates this input buffer.
+        /// </summary>
+        public void Set()
+        {
+            active = true;
+            activeTime = Time.time;
+        }
+
+        /// <summary>
+        /// Activates this input buffer. Uses time as a parameter in case it's used already since Time.time has wonky execution time. Not really worth using tho.
+        /// </summary>
+        public void Set(float time)
+        {
+            active = true;
+            activeTime = time;
+        }
+
+        /// <summary>
+        /// Try to consume the input. 
+        /// </summary>
+        /// <returns>true when input is activated and in an acceptable timeframe.</returns>
+        public bool TryConsume()
+        {
+            if (active && Time.time - activeTime >= bufferDuration)
+            {
+                active = false;
+                return true;
+            }
+            return false;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static implicit operator bool(BufferedInput input)
+        {
+            return input.TryConsume();
+        }
+    }
+}
